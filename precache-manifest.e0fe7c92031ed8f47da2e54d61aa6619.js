a124be3f22d3f3a3663ee060a3e07f8fc013af68
Christina Bergstrom <Christina.d.Bergstrom@gmail.com>
2018-12-30 14:26:51 -0500

    Updates

---
+++ precache-manifest.e0fe7c92031ed8f47da2e54d61aa6619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c4049859fa4acc51ce8d8fdeaaa0bc8",
+    "url": "/myapp/static/media/pinkwatercolor.1c404985.jpg"
+  },
+  {
+    "revision": "69a14def870214bb9966387450a815ec",
+    "url": "/myapp/static/media/KGGirlOnFire.69a14def.ttf"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "287688b6081920b383d4",
+    "url": "/myapp/static/js/main.287688b6.chunk.js"
+  },
+  {
+    "revision": "11951cc86bcd916e1a27",
+    "url": "/myapp/static/js/1.11951cc8.chunk.js"
+  },
+  {
+    "revision": "287688b6081920b383d4",
+    "url": "/myapp/static/css/main.a85c2d6f.chunk.css"
+  },
+  {
+    "revision": "25ce283197424d273915a83d9322709c",
+    "url": "/myapp/index.html"
+  }
+];
